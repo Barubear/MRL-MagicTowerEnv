487321247c96bb5d4387ddbbe467e4485690be8d
Barubear <zxsyxx@gmail.com>
2024-05-19 15:58:31 +0900

    paramater update

--- main.py
+++ main.py
@@ -13,30 +13,29 @@ import torch
 
 import render_test
 import train
-vec_env = make_vec_env("MagicTowerEnv-v0",n_envs=8, monitor_dir="models")
+vec_env = make_vec_env("MagicTowerEnv-v0", n_envs=8,monitor_dir="models")
 model = RecurrentPPO(
     "MlpLstmPolicy",
     vec_env,
     batch_size=1024,
-    n_steps=2048,  # 等价于 buffer_size / batch_size
-    learning_rate=0.0003,
-    ent_coef=0.005,  # 类似于beta
-    clip_range=0.2,  # 类似于epsilon
-    n_epochs=32,
+    n_steps=512,  
+    ent_coef=0.005,  
+    clip_range=0.2,   
+    n_epochs=8,
     gamma=0.995,
     gae_lambda=0.95,
     max_grad_norm=0.5,
     vf_coef=0.5,
     policy_kwargs=dict(
         net_arch=dict(pi=[512, 1024], vf=[512, 1024]),
-        lstm_hidden_size=256,  # 内存大小
+        lstm_hidden_size=512,  # 内存大小
         n_lstm_layers=2
     ),
-    device='cuda' if torch.cuda.is_available() else 'cpu',
+    device='cuda',
     verbose=1
 )
 
-print(train.train(model,vec_env,3000000))
+print(train.train(model,vec_env,2000000))
 model = RecurrentPPO.load("models/best_model" )
 #render_test.render_test(model,vec_env,100) 
 render_test.test(model,vec_env,200) 
