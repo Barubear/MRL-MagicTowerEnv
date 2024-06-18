c198077c1c9d04f8836ecbac9e8a3b4ec7c68974
Barubear <zxsyxx@gmail.com>
2024-06-18 12:44:11 +0900

    Developer_controller test

--- Developer_controller.py
+++ Developer_controller.py
@@ -1,4 +1,17 @@
 class Developer_controller:
 
     def __init__(self,weights) -> None:
+        self.weights = weights
         pass
+
+    def add_weight(self,obs):
+        new_obs =obs.copy()
+        
+
+
+
+
+
+        return new_obs
+        
+
